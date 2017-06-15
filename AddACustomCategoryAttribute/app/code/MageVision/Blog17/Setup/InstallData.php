5bb9b79ef93c8d6524a4e0b5273130b304b4a6f1
Niko K <info@nikolakisae.com>
2017-06-15 12:07:31 +0200

    Blog 17

---
+++ AddACustomCategoryAttribute/app/code/MageVision/Blog17/Setup/InstallData.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * MageVision Blog17
+ *
+ * @category     MageVision
+ * @package      MageVision_Blog17
+ * @author       MageVision Team
+ * @copyright    Copyright (c) 2017 MageVision (https://www.magevision.com)
+ * @license      http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+namespace MageVision\Blog17\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Catalog\Model\Category;
+use Magento\Eav\Setup\EavSetupFactory;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * Eav setup factory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Init
+     *
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        $eavSetup->addAttribute(
+            Category::ENTITY,
+            'seo_description',
+            [
+                'type' => 'text',
+                'label' => 'SEO Description',
+                'input' => 'textarea',
+                'required' => false,
+                'sort_order' => 4,
+                'global' => ScopedAttributeInterface::SCOPE_STORE,
+                'wysiwyg_enabled' => true,
+                'is_html_allowed_on_front' => true,
+                'group' => 'General Information',
+            ]
+        );
+    }
+}
