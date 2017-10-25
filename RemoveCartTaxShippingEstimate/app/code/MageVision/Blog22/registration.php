64adf3cbddc2c18d47d29eb668cb2608f7fd8e25
Nikolas <nikolakisae@gmail.com>
2017-10-25 13:01:00 +0200

    Blog Post #22

---
+++ RemoveCartTaxShippingEstimate/app/code/MageVision/Blog22/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * MageVision Blog22
+ *
+ * @category     MageVision
+ * @package      MageVision_Blog22
+ * @author       MageVision Team
+ * @copyright    Copyright (c) 2017 MageVision (https://www.magevision.com)
+ * @license      http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MageVision_Blog22',
+    __DIR__
+);
