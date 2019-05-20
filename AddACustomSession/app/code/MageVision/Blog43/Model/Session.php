cd8801e48d53c1ef4d5818c851bb7ab2379d948b
Nikolas <nikolakisae@gmail.com>
2019-05-20 12:36:16 +0200

    Blog Post 43

---
+++ AddACustomSession/app/code/MageVision/Blog43/Model/Session.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * MageVision Blog43
+ *
+ * @category     MageVision
+ * @package      MageVision_Blog43
+ * @author       MageVision Team
+ * @copyright    Copyright (c) 2019 MageVision (https://www.magevision.com)
+ * @license      http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+namespace MageVision\Blog43\Model;
+
+use Magento\Framework\Session\SessionManager;
+
+/**
+ * Blog43 session model
+ */
+class Session extends SessionManager
+{
+}
---
+++ AddACustomSession/app/code/MageVision/Blog43/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * MageVision Blog43
+ *
+ * @category     MageVision
+ * @package      MageVision_Blog43
+ * @author       MageVision Team
+ * @copyright    Copyright (c) 2019 MageVision (https://www.magevision.com)
+ * @license      http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MageVision_Blog43',
+    __DIR__
+);
