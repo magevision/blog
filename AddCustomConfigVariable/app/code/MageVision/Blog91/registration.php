69aee50164130a1ed05e5c930aab2b2c0c2f2ecf
Nikolas <nikolakisae@gmail.com>
2024-01-15 11:01:31 +0200

    Blog Post 91

---
+++ AddCustomConfigVariable/app/code/MageVision/Blog91/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * MageVision Blog91
+ *
+ * @category     MageVision
+ * @package      MageVision_Blog91
+ * @author       MageVision Team
+ * @copyright    Copyright (c) 2023 MageVision (https://www.magevision.com)
+ * @license      http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MageVision_Blog91',
+    __DIR__
+);
